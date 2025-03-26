ef61f2d9d1fbd34b1db4c7b66098c1e9054886c5
Andrii Rieznik <andrii.rieznik@pm.me>
2025-03-26 18:25:37 +0100

    feat: aliases (#96)

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,10 @@
+# Put here your custom aliases
+
+alias zshreload="source ~/.zshrc"
+
+# Open the dotfiles repository in Visual Studio Code
+# If no path is set, you can set it with:
+# git config -f "$HOME/.gitconfig" dotfiles.path "$HOME/path/to/dotfiles"
+DOTFILES_PATH=$(git config -f "$HOME/.gitconfig" --get dotfiles.path)
+
+alias dotfiles="code $DOTFILES_PATH"
